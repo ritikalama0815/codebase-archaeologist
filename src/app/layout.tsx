73e62e4ddae290ddb8db7a977e1b2639f19df023
Ritika Lama <ritikatheeng2002@gmail.com>
2026-09-16 19:28:54 -0500

    solved tree hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
       <body>{children}</body>
-    </html>
+    
   );
 }
